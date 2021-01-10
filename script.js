c69325a352a303578d259641a2d334179a2e7fc1
hania-jas <60855635+hania-jas@users.noreply.github.com>
2021-01-10 12:10:40 +0100

    created conditions not allowing for action buttons dupliaction

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
+const plusBtn = document.getElementById('plus');
+const actionBtn = document.getElementsByClassName('action');
+const leftBracket = document.getElementById('leftBracket');
 
+
+const calc = textFromScreen => {
+    return Function('return ' + textFromScreen)();
+}
 const evaluateScreenDisplay = (e) => {
 
     let clickedButton = e.target;
     let clickedValue = clickedButton.innerText;
+    let textFromScreen = output.innerText;
 
-    if (clickedValue === '=') {
-        let textFromScreen = output.innerText;
-        output.innerHTML = eval(textFromScreen);
+    if (clickedValue === '=' && textFromScreen !== '') {
+
+        output.innerHTML = calc(textFromScreen);
+
     } else if (clickedValue === 'C') {
         cleaning();
+
+    } else if (((clickedValue === '+' && textFromScreen.charAt(textFromScreen.length - 1) === '+') || (clickedValue === '-' && textFromScreen.charAt(textFromScreen.length - 1) === '-') || (clickedValue === '/' && textFromScreen.charAt(textFromScreen.length - 1) === '/') || (clickedValue === '*' && textFromScreen.charAt(textFromScreen.length - 1) === '*') || (clickedValue === '.' && textFromScreen.charAt(textFromScreen.length - 1) === '.') || (clickedValue === '(' && textFromScreen.charAt(textFromScreen.length - 1) === '(') || (clickedValue === ')' && textFromScreen.charAt(textFromScreen.length - 1) === ')')) || ((clickedValue === '-' || clickedValue === '+' || clickedValue === '/' || clickedValue === '*' || clickedValue === ')' || clickedValue === '.') && (textFromScreen.charAt(textFromScreen.length - 1) === '+' || textFromScreen.charAt(textFromScreen.length - 1) === '-' || textFromScreen.charAt(textFromScreen.length - 1) === '*' || textFromScreen.charAt(textFromScreen.length - 1) === '/' || textFromScreen.charAt(textFromScreen.length - 1) === '(')) || ((clickedValue === '=' || clickedValue === '-' || clickedValue === '+' || clickedValue === '/' || clickedValue === '*' || clickedValue === ')' || clickedValue === '.') && (textFromScreen === '')) || (((clickedValue === '(' && textFromScreen.charAt(textFromScreen.length - 1) >= '0') || (clickedValue === '(' && textFromScreen.charAt(textFromScreen.length - 1) === ')')) || clickedValue === ')' && !textFromScreen.includes('('))) {
+
+        clickedValue = '';
+
     } else {
         output.innerHTML = output.innerHTML + clickedValue;
     }
+
+
+
+
 }
 
-const cleaning = () => {
+
+function cleaning() {
     output.innerHTML = "";
 }
 
